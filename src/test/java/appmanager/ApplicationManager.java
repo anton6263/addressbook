21b5730228c2341e1e296ac558b78b05622cf850
anton6263 <anton.arteeff@yandex.ru>
2022-05-09 15:40:29 +0300

    Задание №7

--- src/test/java/appmanager/ApplicationManager.java
+++ src/test/java/appmanager/ApplicationManager.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,9 +17,18 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else {
+            wd = new FirefoxDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/index.php");
         contactHelper = new ContactHelper(wd);
