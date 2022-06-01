3d19b0af738f635ff427dce5bbbbb8bd1d29c506
anton6263 <anton.arteeff@yandex.ru>
2022-06-01 22:34:11 +0300

    Задание №9

--- src/test/java/appmanager/ContactHelper.java
+++ src/test/java/appmanager/ContactHelper.java
@@ -80,8 +80,8 @@ public class ContactHelper extends HelperBase {
         List<WebElement> elements = wd.findElements(By.cssSelector("tr[name=entry]"));
         for (WebElement element : elements) {
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            String name = element.findElement(By.xpath("//td[3]")).getText();
-            String lastname = element.findElement(By.xpath("//td[2]")).getText();
+            String name = element.findElement(By.xpath(".//td[3]")).getText();
+            String lastname = element.findElement(By.xpath(".//td[2]")).getText();
             ContactData contact = new ContactData(id, name, lastname, null, null );
             contacts.add(contact);
         }
