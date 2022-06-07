41b77a4163c48aa9576b3d67118a73533a89e03b
anton6263 <anton.arteeff@yandex.ru>
2022-06-08 00:06:06 +0300

    Правка во fluent-интерфейсе

--- src/test/java/appmanager/ContactHelper.java
+++ src/test/java/appmanager/ContactHelper.java
@@ -101,7 +101,7 @@ public class ContactHelper extends HelperBase {
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
             String name = element.findElement(By.xpath(".//td[3]")).getText();
             String lastname = element.findElement(By.xpath(".//td[2]")).getText();
-            ContactData contact = new ContactData(id, name, lastname, null, null );
+            ContactData contact = new ContactData().withId(id).withFirstname(name).withLastname(lastname);
             contacts.add(contact);
         }
         return contacts;
